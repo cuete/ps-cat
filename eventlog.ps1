6c119174760b2aab1d20228e862561ffe7386c70
Alejandro Echeverría <ae@hyperme.net>
2018-01-22 16:06:01 -0800

    Update eventlog.ps1

--- eventlog.ps1
+++ eventlog.ps1
@@ -6,3 +6,4 @@ Get-EventLog -LogName "Application" | Where-Object { $_.InstanceID -like 666 }
 Get-EventLog -LogName "Application" | Where-Object { $_.EntryType -eq "Error" -or $_.EntryType -eq 'Warning' } #Error|Warning|Information
 Get-EventLog -LogName "Application" | Sort-Object "Source" -unique
 Get-EventLog -LogName "System" -ComputerName "RemoteServer" -Newest 10 
+Get-EventLog -ComputerName "RemoteServer" -LogName "Application"  | Where-Object { $_.Message -like "query" -and $_.EntryType -eq "Error" } | Select-Object Source, Message -Last 10
