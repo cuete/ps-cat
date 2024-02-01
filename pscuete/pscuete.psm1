acce30646d7ebd46d1ab92c26a604cfbf836a604
Alejandro Echeverria <alechev@microsoft.com>
2024-01-31 19:04:18 -0800

    Adding display scaling module

--- pscuete/pscuete.psm1
+++ pscuete/pscuete.psm1
@@ -295,3 +295,23 @@ function Write-File
     }
 }
 Export-ModuleMember -Function Write-File
+
+function Set-DisplayScale
+{
+    # scaling = 0 : 100% (default)
+    # scaling = 1 : 125% 
+    # scaling = 2 : 150% 
+    # scaling = 3 : 175% 
+    Param($s=0)
+    $source = @'
+    [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+    public static extern bool SystemParametersInfo(
+                      uint uiAction,
+                      uint uiParam,
+                      uint pvParam,
+                      uint fWinIni);
+'@
+    $apicall = Add-Type -MemberDefinition $source -Name WinAPICall -Namespace SystemParamInfo -PassThru
+    $apicall::SystemParametersInfo(0x009F, $s, $null, 1) | Out-Null
+}
+Export-ModuleMember -Function Set-DisplayScale
